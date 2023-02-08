9e22e9c6211b9d01ff1b0f9095c4badbf8ca1e6f
Wang Khang Waei <devtesting404@gmail.com>
2023-02-08 12:50:15 +0800

    Make backup of fpm-cgi www.conf during docker build

--- srcs/requirements/wordpress/conf/configure.sh
+++ srcs/requirements/wordpress/conf/configure.sh
@@ -22,11 +22,11 @@ if [ $(ls -1A /var/www/ | wc -l) -eq 0 ]; then
 	wp core download --allow-root
 	echo "Creating config"
 	wp core config --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
-	wp config set WP_REDIS_HOST $REDIS_HOST
-	wp config set --raw WP_REDIS_PORT $REDIS_PORT
-	wp config set --raw WP_REDIS_TIMEOUT 1
-	wp config set --raw WP_REDIS_READ_TIMEOUT 1
-	wp config set --raw WP_REDIS_DATABASE 0
+	wp config set --allow-root WP_REDIS_HOST $REDIS_HOST
+	wp config set --allow-root --raw WP_REDIS_PORT $REDIS_PORT
+	wp config set --allow-root --raw WP_REDIS_TIMEOUT 1
+	wp config set --allow-root --raw WP_REDIS_READ_TIMEOUT 1
+	wp config set --allow-root --raw WP_REDIS_DATABASE 0
 	chmod 644 wp-config.php
 	echo "Installing core"
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USERNAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
@@ -36,9 +36,11 @@ if [ $(ls -1A /var/www/ | wc -l) -eq 0 ]; then
 		wp user create $WP_USER $WP_USER_EMAIL --role=$WP_USER_ROLE --user_pass=$WP_USER_PASSWORD --allow-root --quiet
 	fi
 	wp plugin install redis-cache --activate --allow-root
-	wp plugin update --all --allow-root
 	echo "Finished setting up wordpress"
 fi
+
+echo "Updating wordpress plugins"
+wp plugin update --all --allow-root
 
 echo "Setting up fpm-cgi pool config..."
 sed -i "s/^\s*listen\s*=\s*.*/listen = $WP_PORT/g" /etc/php/7.3/fpm/pool.d/www.conf
